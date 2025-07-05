b1244b3b4b4e2d6942e4926cd642d829fca10fa3
Ten-T-Tonmoy <stu01754@gmail.com>
2025-07-05 21:18:30 +0600

    bit of refactoring and its smooth mmmhhh

--- src/pages/Ai/Assistant.jsx
+++ src/pages/Ai/Assistant.jsx
@@ -2,6 +2,12 @@ import { useState, useRef, useEffect } from "react";
 import { useApi } from "../../hooks/useApi";
 import MarkDown from "./Markdown";
 
+import { GiBrain } from "react-icons/gi";
+import { PiBrainFill } from "react-icons/pi";
+import { RiMentalHealthFill } from "react-icons/ri";
+import { RiMentalHealthLine } from "react-icons/ri";
+import { DiAtom } from "react-icons/di";
+
 const Assistant = () => {
   const { messages, sendMessage, loading } = useApi();
   const [input, setInput] = useState("");
@@ -33,8 +39,8 @@ const Assistant = () => {
   return (
     <div className="w-full bg-primary pt-10 pb-20 ">
       <div className="w-[90vw] rounded-xl  m-auto md:w-[60vw]  bg-primary">
-        <h2 className="bg-[#222222] rounded-t-md  text-[1.2rem] font-extrabold p-2 text-center text-white ">
-          ✨ AI Assistant ✨
+        <h2 className="bg-[#222222] justify-center flex items-center gap-2 rounded-t-md  text-[1.2rem] font-extrabold p-2 text-center text-white ">
+          <PiBrainFill /> AI Assistant
         </h2>
 
         {/**chat box here */}
@@ -44,6 +50,14 @@ const Assistant = () => {
         min-h-[400px] border-2 max-h-[400px]
          border-[#ccc] p-3 mb-3"
         >
+          {messages.length === 0 && (
+            <div className="justify-center items-center w-full h-full">
+              <DiAtom className="text-[16rem] m-auto text-white" />
+              <h1 className=" text-center font-bold">
+                How can I help you Today ?
+              </h1>
+            </div>
+          )}
           {messages.map((msg, idx) => (
             <div
               key={idx}
